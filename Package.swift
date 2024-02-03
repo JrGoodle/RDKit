6278ca733a2fbf376ef19cea8b793b2157bd3e11
Joe DeCapo <joe@polka.cat>
2024-02-03 16:45:20 -0600

    Update binary checksum

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let version = "2023.9.4"
 let moduleName = "RDKit"
-let checksum = ""
+let checksum = "a67d7da9744cac7cd6cb3c5062cbd983fda0b6af6af24d0426b9de8445bbe2d2"
 
 let package = Package(
     name: moduleName,
@@ -16,6 +16,9 @@ let package = Package(
             name: moduleName,
             targets: [moduleName]
         )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/JrGoodle/boost", .exact("1.84.0")),
     ],
     targets: [
         .binaryTarget(
